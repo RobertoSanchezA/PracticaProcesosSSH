516190ea6ad8086a30826a63e64c472a35cb511a
roberto <usr2266@salesianos-lacuesta.net>
2022-02-28 19:17:23 +0000

    "Project creation"

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
