c7e9e64b2b82fe22aed56d529030f486e8205fab
roberto <usr2266@salesianos-lacuesta.net>
2022-02-28 19:44:11 +0000

    "Feature: Session created and configured"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
@@ -10,7 +11,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Session session = null;
+        ChannelExec channel = null;
+        Scanner scan = new Scanner(System.in);
+        try{
+            System.out.println("Introduce los siguientes datos:");
+            System.out.println("Nombre de usuario");
+            String username = scan.nextLine();
+
+            System.out.println("Contraseña");
+            String password = scan.nextLine();
+
+            System.out.println("HOST");
+            String host = scan.nextLine();
+
+            System.out.println("Puerto");
+            int port = scan.nextInt();
+
+            session.setConfig("StrictHostKeyChecking", "no");
 
 
+            System.out.println("Configurando session...");
+            session = new JSch().getSession(username, host, port);
+            session.setPassword(password);
+            System.out.println("Conectando");
+            session.connect();
+
+            channel = (ChannelExec) session.openChannel("exec");
+
+
+        } catch (JSchException e) {
+            e.printStackTrace();
+        }
     }
 }
